7753cf8ad54615c602ff8ec2a2376e6e80246f46
CbruceMaryville <37521685+CbruceMaryville@users.noreply.github.com>
2018-03-18 21:18:03 -0500

    Update StarFigures.java

--- src/StarFigures.java
+++ src/StarFigures.java
@@ -1,16 +1,69 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s):Clayton Bruce	
+  	Date: 3/18/2018
 */
 
 // 4. Your pseudocode algorithm for how to break down the figure
+/*
+System print 5star
+System print 5star
+System print " * * "
+System print "  *  :
+System print " * * "
+System print newline
 
+System print 5star
+System print 5star
+System print " * * "
+System print "  *  :
+System print " * * "
+System print 5star
+System print 5star
+System print newline
+
+System print "  *  :
+System print "  *  :
+System print "  *  :
+System print 5star
+System print 5star
+System print " * * "
+System print "  *  :
+System print " * * "
+*/
 
 public class StarFigures {
 
 	public static void main(String[] args) {
 
+	
+		System.out.println("*****");
+		System.out.println("*****");
+		System.out.println(" * * ");
+		System.out.println("  *  ");
+		System.out.println(" * * ");
+		
+		System.out.println("\n\n ");
+
+		System.out.println("*****");
+		System.out.println("*****");
+		System.out.println(" * * ");
+		System.out.println("  *  ");
+		System.out.println(" * * ");
+		System.out.println("*****");
+		System.out.println("*****");
+	
+		System.out.println("\n\n ");
+		
+		System.out.println("  *  ");
+		System.out.println("  *  ");
+		System.out.println("  *  ");
+		System.out.println("*****");
+		System.out.println("*****");
+		System.out.println(" * * ");
+		System.out.println("  *  ");
+		System.out.println(" * * ");
+		
 	}
 
 }
